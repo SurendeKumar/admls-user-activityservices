c3e4f78f3ee3720f26cb17413262e9b1a9a79fb0
SurenderKumar1988 <surender.kumar@otals.com>
2026-04-13 13:56:47 +0100

    generated the springboot package using spring initialiser

---
+++ admls-user-activity-service/src/main/java/com/adlms/useractivityservice/AdmlsUserActivityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adlms.useractivityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmlsUserActivityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmlsUserActivityServiceApplication.class, args);
+    }
+
+}
